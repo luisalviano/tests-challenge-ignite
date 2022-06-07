26a743baf9083cf65cd31e3688a4f7ce80a4eace
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-06-07 16:15:09 -0300

    test: add get balance use case tests

---
+++ src/modules/statements/useCases/getBalance/GetBalanceUseCase.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../../../users/useCases/createUser/CreateUserUseCase";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { GetBalanceError } from "./GetBalanceError";
+import { GetBalanceUseCase } from "./GetBalanceUseCase";
+
+let inMemoryStatementsRepository: InMemoryStatementsRepository;
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let createUserUseCase: CreateUserUseCase;
+let getBalanceUseCase: GetBalanceUseCase;
+
+describe("Get Balance", () => {
+  beforeEach(() => {
+    inMemoryStatementsRepository = new InMemoryStatementsRepository();
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+    getBalanceUseCase = new GetBalanceUseCase(inMemoryStatementsRepository, inMemoryUsersRepository);
+  });
+
+  it("should be able to return a list with all statements and balance from authenticated user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "User Test",
+      email: "user@test.com",
+      password: "1234"
+    });
+
+    const response = await getBalanceUseCase.execute({ user_id: user.id! });
+
+    expect(response).toHaveProperty("statement");
+    expect(response).toHaveProperty("balance");
+  });
+
+  it("should not be able to get balance of nonexistent user", () => {
+    expect(async () => {
+      await getBalanceUseCase.execute({user_id: "nonexistent_id"});
+    }).rejects.toBeInstanceOf(GetBalanceError);
+  });
+});
