fec941ec60b7bddf9f50e8f2200ccd5d74bbb21a
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-06-07 11:36:33 -0300

    test: update create user use case second test

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,5 +1,5 @@
-import { AppError } from "../../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let createUserUseCase: CreateUserUseCase;
@@ -34,7 +34,7 @@ describe("Create User", () => {
         email: "test@test.com",
         password: "1234"
       });
-    }).rejects.toBeInstanceOf(AppError);
+    }).rejects.toBeInstanceOf(CreateUserError);
   })
 
 });
