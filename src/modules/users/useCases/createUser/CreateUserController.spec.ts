4b036f0424e1eba14f6dc26f1d7b53c89edd3e7e
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-06-08 09:49:00 -0300

    test: add create user controller integration tests

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,41 @@
+import request from "supertest";
+import { Connection, createConnection } from "typeorm";
+import { app } from "../../../../app";
+
+let connection: Connection;
+
+describe("Create User Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new user", async () => {
+    const response = await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "User Supertest",
+        email: "user@supertest.com",
+        password: "supertest"
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("should not be able to create a new user with an existing email", async () => {
+    const response = await request(app)
+      .post("/api/v1/users")
+      .send({
+        name: "User Supertest",
+        email: "user@supertest.com",
+        password: "supertest"
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
